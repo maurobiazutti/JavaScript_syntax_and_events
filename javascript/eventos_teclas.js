a706cffa8d595e04ba7f36d70978c7cc26ad5417
mauro <mauro_biazutti@hotmail.com>
2023-10-20 16:03:45 -0300

    evento de teclas

---
+++ javascript/eventos_teclas.js
@@ -0,0 +1,21 @@
+// onkeydown -> Dispara quando a tecla é precionada
+function teclaPressionadaESolata(){
+  let input2 = document.getElementById('input2').value;
+  console.log(input2)
+}
+
+
+// onkeypress -> Dispara quando a tecla é pressionada e solta
+function teclaPressionada(){
+  let input1 = document.getElementById('input1').value;
+  console.log(input1)
+}
+
+
+
+// onkeyup -> Dispara quando a tecla é solta sobre um elemento
+
+function teclaPressionadaESolataUp(){
+  let input3 = document.getElementById('input3').value;
+  console.log(input3)
+}
